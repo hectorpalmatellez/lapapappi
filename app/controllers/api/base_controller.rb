9fcd5b944b1646016554aa2ce4e6e5ea4b4d5f02
Choon-ho Yoon <choonho.yoon.b@gmail.com>
2015-09-11 15:37:04 -0300

    Move Protect From Forgery to Base Controller

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,6 @@
 class Api::BaseController < ApplicationController
+  protect_from_forgery except: :create
+
   def current_resource_owner
     User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,5 @@
 class Api::UsersController < Api::BaseController
   after_filter :cors_set_access_control_headers
-  protect_from_forgery except: :create
 
   def create
     user = User.create(user_params)
